32358ab5a38d798bec3220ea35c792411b3592bf
Azaro68 <104309041+Azaro68@users.noreply.github.com>
2022-05-22 17:26:17 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,147 +1,47 @@
-from pygame import * 
-player_image = 'img/hero/m1.png'
-img_file_enemy = "img/enemy.png.png"
-win_width = 800
-win_height = 600
-window = display.set_mode((win_width,win_height))
-backg = 'img/back.png'
-display.set_caption('Arcade')
-background = transform.scale(image.load(backg),(win_width,win_height))
-C_WHITE = (255, 255, 255)
-C_DARK = (48, 48, 0)
-C_YELLOW = (204,204,204)
-C_GREEN = (63,113,77)
-C_BLUE = (20, 147, 181)
-C_RED = (255, 0, 0)
-C_BLACK = (0, 0, 0)
-walkRight = [image.load('img/hero/right_1.png'),
-image.load('img/hero/right_2.png'),
-image.load('img/hero/right_3.png'),
-image.load('img/hero/right_4.png'),
-image.load('img/hero/right_5.png'),
-image.load('img/hero/right_6.png'),
-image.load('img/hero/right_7.png'),
-image.load('img/hero/right_8.png'),
-image.load('img/hero/right_9.png'),
-image.load('img/hero/right_10.png'),
-image.load('img/hero/right_11.png'),
-image.load('img/hero/right_12.png')]
-img_counter = 0
-player_width = 80
-walkLeft = [image.load('img/hero/left_1.png'),
-image.load('img/hero/left_2.png'),
-image.load('img/hero/left_3.png'),
-image.load('img/hero/left_4.png'),
-image.load('img/hero/left_5.png'),
-image.load('img/hero/left_6.png'),
-image.load('img/hero/left_7.png'),
-image.load('img/hero/left_8.png'),
-image.load('img/hero/left_9.png'),
-image.load('img/hero/left_10.png'),
-image.load('img/hero/left_11.png'),
-image.load('img/hero/left_12.png')]
-class Gamesprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, x_speed, y_speed):
-        super().__init__()
-        self.image = transform.scale(image.load(player_image),(size_x, size_y))
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-        self.x_speed = x_speed
-        self.y_speed = y_speed
-
-    def update(self):
-        keys = key.get_pressed()
-        if keys[K_LEFT]:
-            self.x_speed = -5
-        elif keys[K_RIGHT]:
-            self.x_speed = 5
-        elif keys[K_UP]:
-            self.jump()
-        else:
-            self.x_speed = 0
-        self.rect.x += self.x_speed
-        self.gravitate()
-        self.rect.y += self.y_speed
-        platforms_touched = sprite.spritecollide(self, barriers, False)
-        if self.y_speed > 0:
-            for p in platforms_touched:
-                self.y_speed = 0
-
-                if p.rect.top < self.rect.bottom:
-                    self.rect.bottom = p.rect.top 
-                    self.stands_on = p
-        if self.y_speed < 0:
-            self.stands_on = False
-            for p in platforms_touched:
-                self.y_speed = 0
-                if p.rect.bottom > self.rect.top:
-                    self.rect.top = p.rect.bottom
-        platforms_touched = sprite.spritecollide(self, barriers, False)
-        if self.x_speed > 0:
-            for p in platforms_touched:
-                self.rect.right = min(self.rect.right, p.rect.left)
-        elif self.x_speed < 0:
-            for p in platforms_touched:
-                self.rect.left = max(self.rect.left, p.rect.right)
-            
-    def gravitate(self):
-        self.y_speed += 0.25
-
-    def jump(self):
-        if self.stands_on:
-            self.y_speed = -10
+    if robin.x_speed > 0:
+        robin.image = pygame.transform.scale(walkRight[img_counter//3], (player_width, 120)).convert_alpha()
+    elif robin.x_speed < 0:
+        robin.image = pygame.transform.scale(walkLeft[img_counter//3], (player_width, 120)).convert_alpha()
+    else:
+        robin.image = pygame.transform.scale(playerStand[img_counter//6], (player_width, 120)).convert_alpha()
+    img_counter += 1
+    all_sprites.draw(window)
  
-    def draw_hero(self):
-        global img_counter
-        if img_counter == 36:
-            img_counter = 0
-        if self.x_speed > 0:
-            self.image = transform.scale(walkRight[img_counter//3], (player_width, 120))
-        elif self.x_speed < 0: 
-            self.image = transform.scale(walkLeft[img_counter//3], (player_width, 120))
-            
-        img_counter +=1 
-        window.blit(self.image,(0,78))
-
-class Enemy(Gamesprite):
-    side = 'left'
+class Enemy(GameSprite): 
+    side = "left"
     def update(self):
         if self.rect.x <= 0:
-            self.side = 'right'
+            self.side = "right"
         if self.rect.x >= win_width /2:
             self.side = "left"
         if self.side == "left":
-            self.rect.x -= self.x_speed
-        else: 
-            self.rect.x += self.x_speed 
-class Wall(sprite.Sprite):
-    def __init__(self, x, y, width, height, color=C_GREEN):
-        super().__init__()
-        self.image = Surface([width, height])
-        self.image.fill(color)
-
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-    
-enemies = sprite.Group()
-bombs = sprite.Group()
-all_sprites = sprite.Group()
-barriers = sprite.Group()
-en = Enemy(img_file_enemy, 50, 480, 110, 110, 2, 0)
-all_sprites.add(en)
-enemies.add(en)
-en = Enemy(img_file_enemy, 400, 480, 110, 110, 2, 0)
-all_sprites.add(en)
-enemies.add(en)
-pr = Gamesprite(img_file_princess, win_width + 500, win_height - 125, 80, 120, 0)
-all_sprites.add(pr)
-robin = Gamesprite(player_image,50,50,80,120,12,12)
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+ 
+# Запуск игры
+pygame.display.set_caption("ARCADA") 
+window = pygame.display.set_mode([win_width, win_height])
+ 
+background = pygame.transform.scale(pygame.image.load(img_file_back).convert(), (win_width, win_height)) 
+ 
+# список всех персонажей игры:
+all_sprites = pygame.sprite.Group()
+# список препятствий:
+barriers = pygame.sprite.Group()
+# список врагов:
+enemies = pygame.sprite.Group()
+# список мин:
+bombs = pygame.sprite.Group()
+ 
+# создаем персонажа, добавляем его в список всех спрайтов:
+robin = Hero(img_file_hero) 
+all_sprites.add(robin)
+# создаем стены, добавляем их:
 w = Wall(50, 150, 480, 20)
 barriers.add(w)
 all_sprites.add(w)
-w = Wall(700,0,30,400)
+w = Wall(700, 0, 30, 400) 
 barriers.add(w)
 all_sprites.add(w)
 w = Wall(350, 380, 640, 20)
@@ -150,18 +50,4 @@ all_sprites.add(w)
 w = Wall(-200, 590, 1600, 20)
 barriers.add(w)
 all_sprites.add(w)
-finish = False
-game = True
-
-
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    if not finish:
-        all_sprites.update()
-    window.blit(background,(0,0))
-    robin.draw_hero()
-    robin.update()
-    display.update()
-    time.delay(20)
+ 
